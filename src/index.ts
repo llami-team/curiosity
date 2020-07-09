e5835ededf05b37adbc2ca66f3c80fd47627b87a
hmmhmmhm <hmmhmmhm@naver.com>
2020-07-09 17:55:42 +0900

    Modeling Tes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-import { } from './layer1'
+import * as Layer1 from './layer1'
 
-// One step
+// ! 사과의 본질 생성 시작
+// * 사과 본질의 별칭을 색인합니다.
+const appleAlias: Layer1.IAlias = {
+    id: 0,
+    names: [
+        '사과',
+    ]
+}
+
+// * 사과의 본질을 모델화합니다.
+const appleEssence: Layer1.IEssence = {
+    alias: appleAlias,
+    relations: [],
+    states: [],
+    types: [],
+}
+
+// ! 논리 표현 목표대상 -> 사과는 과일이다
+// * 과일 본질의 별칭을 색인합니다.
+const fruitAlias: Layer1.IAlias = {
+    id: 1,
+    names: [
+        '과일',
+    ]
+}
+
+// * 과일의 본질을 모델화합니다.
+const fruitEssence: Layer1.IEssence = {
+    alias: fruitAlias,
+    relations: [],
+    states: [],
+    types: [],
+}
+
+// * "사과는 과일이다"
+const appleType: Layer1.IType = {
+    alias: fruitAlias,
+    essence: fruitEssence,
+}
+appleEssence.types.push(appleType)
--- src/layer1/language/interface/index.ts
+++ src/layer1/language/interface/index.ts
@@ -1,2 +1,7 @@
 export * from './alias'
+export * from './condition'
+export * from './essence'
+export * from './existence'
+export * from './relation'
 export * from './state'
+export * from './type'
